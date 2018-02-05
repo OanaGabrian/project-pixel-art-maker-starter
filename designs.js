09d5e4aeb0fdbf3469670b11355b74ab65e406ee
OanaGabrian <34296403+OanaGabrian@users.noreply.github.com>
2018-02-05 14:55:49 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,35 @@
-// Select color input
-// Select size input
+var gridCreate = $('#pixel_canvas');
 
-// When size is submitted by the user, call makeGrid()
+$('input:submit').click(function(event){
+  event.preventDefault();
+  makeGrid();
+});
 
+// gridCreate 
 function makeGrid() {
+  gridCreate.empty();
+  let gridHeight = document.getElementById("input_height").value;
+  let gridWidth = document.getElementById("input_width").value;
+  for (var s = 0; s < gridHeight; s ++ ) {
+    gridCreate.append('<tr class="' + s + '">', '</tr>');
+    for (var n = 0; n < gridWidth; n ++) {
+      $('.' + s).append('<td>', '</td>');
+    }
+  }
+};
 
-// Your code goes here!
+// Background color 
+gridCreate.on("mousemove", "td", function() {
+  var colorInput = document.getElementById("colorPicker").value;
+  if($(this).hasClass('mousemove')) {
+  } else {
+    $(this).css("background-color", colorInput);
+  }
+  
+  //reset grid 
+  var resetGrid = $('#reset-grid');
+  resetGrid.on('click', function() {
+    $('td').css('background-color', '');
+  });
 
-}
+});
